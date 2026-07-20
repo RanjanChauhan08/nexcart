f35deae8e1775f09dcf610c80d7ca0435d490574
Ranjan Chauhan <abcranjan888@gmail.com>
2026-07-21 02:59:41 +0530

    Add Pillow and set default auto field

--- temp1/settings.py
+++ temp1/settings.py
@@ -93,6 +93,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'temp1.wsgi.application'
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
