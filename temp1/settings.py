aac6d06eff0c5432eaea6a641474b82aa5a27844
Ranjan Chauhan <abcranjan888@gmail.com>
2026-07-24 23:52:53 +0530

    FIX: Remove invalid ssl_require from SQLite database config

--- temp1/settings.py
+++ temp1/settings.py
@@ -121,8 +121,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        ssl_require=IS_PRODUCTION
+        conn_max_age=600
     )
 }
 
